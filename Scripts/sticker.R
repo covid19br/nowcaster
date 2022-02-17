916fdcc029f2bd28713b413f25cea4d48c904ec8
rafalopespx <lopes1313@gmail.com>
2022-02-17 09:24:51 -0300

    a even more beautiful hexsticker

--- Scripts/sticker.R
+++ Scripts/sticker.R
@@ -7,8 +7,8 @@ library("RColorBrewer")
 library("transformr")
 library("vroom")
 
-casos <- vroom("../../dados_processados/integridade_SIVEP/dados_srag_br.csv")
-obs <- read_csv("../../dados_processados/integridade_SIVEP/dados_obsrag_br.csv")
+casos <- vroom("../central_covid/dados_processados/integridade_SIVEP/dados_srag_br.csv")
+obs <- read_csv("../central_covid//dados_processados/integridade_SIVEP/dados_obsrag_br.csv")
 
 casos<-casos %>%
   filter(dt_sin_pri >=  "2020-01-01")
@@ -22,7 +22,7 @@ Sys.setlocale(category = "LC_TIME", locale = "pt_BR.UTF-8")
 ## Gif casos ##
 nowc <-
   ggplot(data = casos, aes(x = dt_sin_pri, y = n, col = as.factor(data))) +
-  geom_line()+
+  geom_line(size = .3)+
   scale_color_viridis_d(direction = -1,
                         aesthetics = "colour",
                         option = "turbo") +
@@ -36,5 +36,5 @@ library(hexSticker)
 
 sticker(nowc, package="nowcaster", 
         h_color="#f9690e", p_color = "#f9690e", h_fill="white", h_size = .3,
-        p_size=18, s_x=.95, s_y=1.2, s_width=2, s_height=1.5,
+        p_size=18, s_x=.95, s_y=1.2, s_width=2, s_height=1.5, dpi = 300,
         filename="man/figures/nowcaster.png")
