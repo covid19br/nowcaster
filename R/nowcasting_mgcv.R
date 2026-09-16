065a65bf66f78affc576c71a12c5997de8cfb203
Leo Bastos <lsbastos@gmail.com>
2026-09-16 15:28:10 -0300

    fixing wrong dataset name

--- R/nowcasting_mgcv.R
+++ R/nowcasting_mgcv.R
@@ -225,7 +225,7 @@ nowcasting_mgcv <- function(dataset,
 
   ## Auxiliary date table
 
-  dates <- range(data.inla |> dplyr::pull(var = date_onset - 7*trim.data))
+  dates <- range(data2model |> dplyr::pull(var = date_onset - 7*trim.data))
 
   # Adding forecast
   dates[2] <- dates[2] + 7*K
